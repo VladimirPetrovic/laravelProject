545e2eb16ff7eeb2d6fe0c2cea6080485f950716
vladimirp <petrovic.vladimir@gmail.com>
2015-03-24 13:41:07 +0100

    Dodao Time Added

--- test/resources/views/article/index.blade.php
+++ test/resources/views/article/index.blade.php
@@ -2,44 +2,54 @@
 
 @section('content')
 
-    <a href="{{ route('article.create')}}" class='btn btn-success'>Add new</a>
+    <div >
 
-    <div class="row">
-       <br><table class="table" class="table-bordered" class="table-responsive">
-                     <tr>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Email</th>
+    <h1><i class="fa fa-users"></i> Articles</h1>
+       <table class="table" class="table-bordered" class="table-responsive">
 
-                    <tr>
+            <thead>
+                 <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Email</th>
+                    <th>Date/Time Added</th>
 
-        @foreach($articles as $article)
-            <div class="col-md-12">
-                        <td class="info">
-                            {{ $article->title }}
-                        </td>
-                        <td class="active">
-                            {{ $article->body }}
-                        </td>
-                        <td class="active">
-                            {{ $article->user->email }}
-                        </td>
-                        <td class="active">
-                            {!! Form::open(['route' => ['article.destroy', $article->id], 'method' => 'DELETE']) !!}
-                            {!! Form::submit('DELETE', ['class' => 'btn btn-danger']) !!}
-                            {!! Form::close() !!}
-                        </td>
-                        <td class="active">                        
-                            {!! Form::open(['route' => ['article.edit', $article->id], 'method' => 'GET' ]) !!}
-                            {!! Form::submit('Edit', ['class' => 'btn btn-default']) !!}
-                            {!! Form::close() !!}
-                        </td>
-                    </tr>
-                
-            </div>
-        @endforeach    
+                <tr>
+            </thead>
+
+                @foreach($articles as $article)
+                    <div class="col-md-12">
+                                <td class="info">
+                                    {{ $article->title }}
+                                </td>
+                                <td class="active">
+                                    {{ $article->body }}
+                                </td>
+                                <td class="active">
+                                    {{ $article->user->email }}
+                                </td>
+                                <td>
+                                    {{ $article->created_at->format('F d, Y h:ia') }}
+                                </td>
+                                <td class="active">
+                                    {!! Form::open(['route' => ['article.edit', $article->id], 'method' => 'GET' ]) !!}
+                                    {!! Form::submit('Edit', ['class' => 'btn btn-primary']) !!}
+                                    {!! Form::close() !!}
+                                    
+                                </td>
+                                <td class="active">                        
+                                    {!! Form::open(['route' => ['article.destroy', $article->id], 'method' => 'DELETE']) !!}
+                                    {!! Form::submit('DELETE', ['class' => 'btn btn-danger']) !!}
+                                    {!! Form::close() !!}
+                                </td>
+                            </tr>
+                        
+                    </div>
+                @endforeach    
         </table>
+        <a href="{{ route('article.create')}}" class='btn btn-success'>Add new</a>
     </div> 
+
 
 @endsection
 
